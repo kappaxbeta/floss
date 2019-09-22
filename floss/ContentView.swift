c1fac6f54578c480dfdeafdf8c15d37772689c65
Kappa x Beta <kappaxbeta@me.com>
2019-09-22 06:42:25 +0200

    Initial Commit

---
+++ floss/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  floss
+//
+//  Created by Jens Bösche on 22.09.19.
+//  Copyright © 2019 Jens Bösche. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
